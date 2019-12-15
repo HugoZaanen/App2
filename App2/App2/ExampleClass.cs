6a894da15e52c45c22ce4f611838ff9d9e5b148b
Hugo Zaanen <HugoZaanen@hotmail.com>
2019-12-15 22:42:47 +0100

    sawa

---
+++ App2/App2/ExampleClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App2
+{
+    class ExampleClass
+    {
+        private String Name { set; get; }
+
+        public ExampleClass()
+        {
+
+        }
+
+        public List<String> ReturnNameList()
+        {
+
+            return null;
+        }
+    }
+}
